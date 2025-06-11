474ab587bde0d072a84d44eb6a3cad6541a5555a
Sanju1234570 <sanjanamoolya2005@gmail.com>
2025-06-11 12:21:20 +0530

    save as

---
+++ Temp.java
@@ -0,0 +1,10 @@
+public class Temp {
+public static void main(String []args) {
+ int a=10, b=15;
+ System.out.println("Before sawp:a="+a+",b=+b");  
+ int temp=a;
+ a=b;
+ temp=b;
+ System.out.println("After sawp:a="+a+",b=+b");       
+}                  
+ }
